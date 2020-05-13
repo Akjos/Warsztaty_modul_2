dfa77495eaf5ddeeba8b53557580a7a407ff8ec5
Akjos <sojka_p@o2.pl>
2020-05-13 07:00:04 +0200

    main change

--- src/com/coderslab/Main.java
+++ src/com/coderslab/Main.java
@@ -5,8 +5,29 @@ import com.coderslab.controllers.MenuManager;
 import com.coderslab.utils.InitDatabase;
 
 public class Main {
+    public void newClass {
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+        System.out.println("dupa");
+    }
     public static void main(String[] args) {
 //        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+//        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+//        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+//        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+//        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+//        new InitDatabase().initDatabase(); /*inicjalizacja bazy danych z przykładowymi wartościami*/
+        new MenuManager().start(args);
+        new MenuManager().start(args);
+        new MenuManager().start(args);
+        new MenuManager().start(args);
+        new MenuManager().start(args);
         new MenuManager().start(args);
 
     }
